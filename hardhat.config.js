9c50965cde2b9d2963339309fe2063a715be4a94
PGWan <PGWan@gmail.com>
2025-07-12 23:38:51 +0800

    update scripts

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,8 +2,9 @@
 
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
+require("@nomicfoundation/hardhat-verify");
 
-const {ALCHEMY_API_URL, SEPOLIA_PRIVATE_KEY} = process.env;
+const {ALCHEMY_API_URL, SEPOLIA_PRIVATE_KEY, ETHERSCAN_API_KEY} = process.env;
 
 module.exports = {
   solidity: "0.8.28",
@@ -12,7 +13,16 @@ module.exports = {
   networks: {
     sepolia: {
       url: ALCHEMY_API_URL,
-      accounts: [SEPOLIA_PRIVATE_KEY]
+      accounts: [SEPOLIA_PRIVATE_KEY],
+      chainId: 11155111,
     }
-  }
+  },
+
+
+  etherscan:{
+    apiKey: {
+      sepolia: ETHERSCAN_API_KEY,
+    },
+  },
+
 };
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,24 +1,46 @@
 
-const {ethers} = require("hardhat");
+const {ethers, network, run} = require("hardhat");
 
 async function main() {
 
-    // 部署对象
-    const [deployer] = await ethers.getSigners();
-
-    console.log("Deploying contracts with the account:", deployer.address);
 
     const ContractFactory = await ethers.getContractFactory("Token");
+    console.log("Contract start to deploying...");
+
     // 部署
     const contract = await ContractFactory.deploy();
 
-    // 等待部署
-    contract.waitForDeployment();
+    // 等待部署完成
+    await contract.waitForDeployment();
 
-    console.log("Deployed Success, Contract address:" ,contract.target);
+    console.log("Deployed success, contract address:" ,contract.target);
 
+    // 验证合约
+    await _verifyContract(contract);
 }
 
+
+async function _verifyContract(contract){
+
+    // 只有Sepolia网络才去验证
+    if(process.env.ETHERSCAN_API_KEY && network.config.chainId == 11155111) {
+        console.log("Waiting for 5 confirmations");
+
+        // 因为区块延时问题，等待一定区块后再去验证合约
+        await contract.deploymentTransaction().wait(5);
+
+        // 验证合约
+        await run("verify:verify", {
+            address: contract.target,
+            constructorArguments: [],
+        });
+    }else{
+        console.log("Verifycation skiped...");
+    }
+
+    
+
+}
 
 main()
 .then(()=> process.exit(0))
